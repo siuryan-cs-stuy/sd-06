55af675fd90f9558b82239a83dcf0480bcb8655f
Ryan Siu <rsiu@stuy.edu>
2017-10-02 14:39:06 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    response = None
+    if request.method == 'POST':
+        response = request.args['textbox']
+    return render_template('index.html', request_method = request.method, response = response)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
